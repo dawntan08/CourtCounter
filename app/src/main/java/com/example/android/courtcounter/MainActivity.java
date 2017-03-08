e216ae63e3293050c387874bc58fd719ef8b0c99
Dawn Tan <dawntan08@gmail.com>
2017-03-08 22:46:24 +1100

    initial button click requirement

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
+    }
+
+
+
+    // initial req: after click button, score change to 3
+    public void scorePlusThree(View view) {
+        displayForTeamA(3);
+    }
+
+    // initial req: after click button, score change to 2
+    public void scorePlusTwo(View view) {
+        displayForTeamA(2);
+    }
+
+    // initial req: after click button, score change to 1
+    public void scorePlusOne(View view) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
